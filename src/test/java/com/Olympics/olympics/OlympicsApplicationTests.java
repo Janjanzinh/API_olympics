f836d3595a023ffd592b3e9d4f7406bf2b70e69d
janjanzinh <janilson1571@gmail.com>
2024-08-19 13:48:06 -0300

    Arquivos da api

---
+++ src/test/java/com/Olympics/olympics/OlympicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Olympics.olympics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlympicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
